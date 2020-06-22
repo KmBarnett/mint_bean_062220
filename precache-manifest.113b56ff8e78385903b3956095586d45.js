45cb59d0152b9910587f4c7f5fd3f0edeafd0575
kmbarnett <kmbarnett94@gmail.com>
2020-06-22 13:30:32 -0600

    Updates

--- precache-manifest.113b56ff8e78385903b3956095586d45.js
+++ precache-manifest.113b56ff8e78385903b3956095586d45.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bacb7a6090338eca81ea400309c41dc0",
+    "revision": "155008834b9f1ebd863ebfc2d1b42bbb",
     "url": "/mint_bean_062220/index.html"
   },
   {
-    "revision": "75765f151b93b195132b",
-    "url": "/mint_bean_062220/static/css/main.d0c6646b.chunk.css"
+    "revision": "50fa7c775a2f53034bc4",
+    "url": "/mint_bean_062220/static/css/main.5315419c.chunk.css"
   },
   {
     "revision": "2c05b268a21dd181cc40",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mint_bean_062220/static/js/2.eab3f23b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "75765f151b93b195132b",
-    "url": "/mint_bean_062220/static/js/main.6a18593b.chunk.js"
+    "revision": "50fa7c775a2f53034bc4",
+    "url": "/mint_bean_062220/static/js/main.be1ce396.chunk.js"
   },
   {
     "revision": "711fb63f847fa0f37f0d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mint_bean_062220/precache-manifest.64b0d74f01c2ea2d15c269ce711cbdca.js"
+  "/mint_bean_062220/precache-manifest.113b56ff8e78385903b3956095586d45.js"
 );
 
 self.addEventListener('message', (event) => {
